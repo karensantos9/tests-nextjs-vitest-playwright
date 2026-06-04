c39afb780022a4302d6ff4df18943a0329ea4290
Karen <karen.santos946@hotmail.com>
2026-06-03 22:37:22 -0300

    configs ambiente e .git

--- src/utils/sanitize-str.ts
+++ src/utils/sanitize-str.ts
@@ -1,5 +1,10 @@
+import { getFullEnv } from "@/env/configs";
+
 export function sanitizeStr(s: string): string {
   return !s || typeof s !== 'string' ? '' : s.trim().normalize();
   // se a string estiver vazia ou não for do tipo string - return string vazia
   // else tiver valor e for string - return string faz trim(limpa as pontas esquesda e direita) e normaliza os caracteres
 }
+
+const env = getFullEnv()
+console.log(env)
